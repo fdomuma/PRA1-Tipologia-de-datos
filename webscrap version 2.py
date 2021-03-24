4cb12c1f3c1b83e23e3d1bbe6724148d579756ca
Ricardop1 <47947522+Ricardop1@users.noreply.github.com>
2021-03-24 17:13:38 +0100

    Se ha añadido Selenium
    Se ha añadido Selenium

--- webscrap version 2.py
+++ webscrap version 2.py
@@ -2,8 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+import selenium
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+import time
+from selenium.webdriver.common.keys import Keys
 
-webBase = "https://www.museodelprado.es/coleccion/obras-de-arte"
+
+def insertNone(datos):
+    """
+    Función para tratar los datos nulos de cada obra
+
+    :param datos: lista con los datos actuales de la obra
+    :return: lista con todas las variables 
+    """
+    columnas = ["Número de catálogo","Autor","Título","Fecha",
+    "Técnica","Soporte","Dimensión","Serie","Procedencia"]
+    for col in columnas:
+        if col not in datos:
+            indice = columnas.index(col)*2
+            datos.insert(indice,col)
+            datos.insert(indice + 1,"None")
+    return datos        
 
 def getPage(srcUrl):
     """
@@ -35,12 +56,31 @@ def getLinks(pageCont):
 
     return links
 
+def getLinksMax(pageCont,max):
+    """
+    Extrae los enlaces url dentro de la clase "presentacion-mosaico" de una web a partir de su estructura HTML.
+    Contiene un numero mázimo de links
+
+    :param pageCont: Estructura HTML de una web.
+    :param max: número maximo de links
+    :return: lista con los enlaces url mencionados
+    """
+
+    mosaico = pageCont.find_all(class_="presentacion-mosaico")
+    links = []
+    len(mosaico)
+    for i in range(max):
+        prueba = mosaico[i].find('a')
+        links.append(prueba.get("href"))
+
+    return links
+
 
 def getDatos(pageCont):
     """
     Extrae los datos incluidos en la ficha técnica
 
-    :param pageCont: Estructura HTML de una web
+    :param pageCont: Estructura HTML de una web   14/15
     :return: Lista con el contenido de la ficha técnica
     """
     #TODO Arreglar la estructura en la que extrae la información
@@ -58,12 +98,17 @@ def getDatos(pageCont):
         else:
             for z in elemento.stripped_strings: # Etiqueta
                 elementos.append(z)
+    #añadimos las columnas pendientes, columna de Url y obteemos el link
+    elementos = insertNone(elementos)
+    elementos.append("UrlImagen")        
+    elementos.append(getLinkImage(pageCont))     
     return elementos
 
 
 #función para descargar imagen
 def load_requests(source_url):
     """
+    Descarga imagen contenida en source_url
     #TODO
     :param source_url:
     :return:
@@ -80,70 +125,82 @@ def load_requests(source_url):
         output.close()
 
 
-def getImage(pageCont):
+def getLinkImage(pageCont):
     """
-
+    Extrae el link de imagende la obra contenida en pageCont
     :param pageCont:
-    :return:
+    :return: Link de la imagen de la obra
     """
-    images = []
-    imagen = pageCont.find(class_="section-viewer")
+    linksImagen = []
+    imagenInfo = pageCont.find(class_="section-viewer")
     #obtenemos todos los img-src
-    for img in imagen.findAll('img'):
-        images.append(img.get('src'))
+    for img in imagenInfo.findAll('img'):
+        linksImagen.append(img.get('src'))
 
-    #descargamos images[1] porque images[0] es None
-    load_requests(images[1])
+    return  list(filter(None, linksImagen))[0]
+
+#-------------------------------------------------------------------------------
+
+#iniciamos conexion al webdriver de selenium 
 
 
+option = webdriver.ChromeOptions()
+option.add_argument("--headless") #opcion para que no aparezca el navegador
+
+#Install Driver
+driver = webdriver.Chrome(ChromeDriverManager().install(), options = option)
+
+webBase = "https://www.museodelprado.es/coleccion/obras-de-arte"
+driver.get(webBase)
+
+driver.find_element_by_tag_name('body').send_keys(Keys.END) # Función que permite llegar al final de la pagina web
+print("Fin pagina")
+time.sleep(10)
+print("Fin time")
+
+#obtenemos el archivo html para beautifulsoup
+body = driver.execute_script("return document.body")
+source = body.get_attribute('innerHTML')
 
 # Extraemos la estructura de la página base
-pagBaseStr = getPage(webBase)
+pagBaseStr = BeautifulSoup(source, "html.parser")
 
 # Extraemos los links
-enlacesObras = getLinks(pagBaseStr)
+numMax = 200
+enlacesObras = getLinksMax(pagBaseStr,numMax)
+driver.close()
 
 # Extraemos los datos
 datos = []
+inicio = 1
 for link in enlacesObras:
-    tempPage = getPage(link)
+    print(inicio,"/",numMax)
+    tempPage = getPage(link)    
     tempData = getDatos(tempPage)
     datos.append(tempData)
+    inicio += 1
+
+
+
 
 datosNP = np.array(datos)
 
 for i, _array in enumerate(datosNP):
     if i == 0: # Sólo se va a ejecutar en la primera iteración luego iniciamos el df
-        tempData = np.reshape(datosNP[i], (9, 2)) # Lista de listas
+        tempData = np.reshape(datosNP[i], (10, 2)) # Lista de listas
         fichaTec = tempData[:, 1] # Extrae la info
         datosDF = pd.DataFrame(data=[fichaTec], columns=tempData[:, 0])
     else:
-        tempData = np.reshape(datosNP[i], (9, 2))
+        tempData = np.reshape(datosNP[i], (10, 2))
         fichaTec = []
         for element in tempData:
             fichaTec.append(element[1])
         datosDF.loc[len(datosDF.index)] = fichaTec
-print(datosDF)
-datosDF.to_csv('mydataframe.csv', index=False, encoding="iso-8859-1") #TODO al exportar el ; corta la línea
+#print(datosDF)
+datosDF.to_csv('mydataframe.csv', index=False, encoding="utf-8") #TODO al exportar el ; corta la línea
 
 
 #TODO hacer una lista con todos los links de descarga e incluirlos en una nueva columna "url" en el df
-"""
-#obtenemos todos los links a buscar
-linksBuscar = getLinks(webBase)
 
-
-#obtenemos los datos de cada link
-datos = []
-for v in linksBuscar:
-   datos.append(getDatos(v))
-
-   #obtenemos la imagen
-   getImage(v)
-
-#imprimimos el codigo de cada pieza
-for d in datos:
-    print(d[1])
-"""
 
 
